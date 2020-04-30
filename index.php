a5b1748ee5e2d6666ac154f152a294b4376d91e6
Sohail Mahmud <46453392+sohailmahmud@users.noreply.github.com>
2020-04-30 23:59:11 +0600

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,6 @@
 	<link rel="stylesheet" type="text/css" href="assets/mycustom/css/styles.css">
 </head>
 <body class="index-body">
-
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
